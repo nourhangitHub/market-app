dda264ec3037889d3b995e3ad04f2199e4c4e113
nourhangitHub <nourhankabsha@gmail.com>
2019-12-22 01:56:51 +0400

    create service

--- src/providers/todoservice/todoservice.ts
+++ src/providers/todoservice/todoservice.ts
@@ -1,5 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { Http , Response} from '@angular/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Observable';
+
 
 /*
   Generated class for the TodoserviceProvider provider.
@@ -8,10 +13,31 @@ import { Injectable } from '@angular/core';
   and Angular DI.
 */
 @Injectable()
+
+
 export class TodoserviceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello TodoserviceProvider Provider');
+private apiUrl : string="https://5bcce576cf2e850013874767.mockapi.io/task/categories";
+
+  constructor(public http: Http) {
+  }
+
+  getData(){
+    return this.http.get(this.apiUrl).do(this.logResponse).
+    map(this.extractData).catch(this.catchError);
+  }
+
+  private catchError( error : Response | any){
+    console.log(error);
+    return Observable.throw(error.json.error ||"server error")
+  }
+
+  private logResponse( res: Response){
+    console.log(res);
+  }
+
+  private extractData( res: Response){
+    return res.json
   }
 
 }
